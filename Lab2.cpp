49f3c6b030e04bd7e2dc8c6b0145b4aa8c99b048
whymerepository <71703045+whymerepository@users.noreply.github.com>
2020-09-22 19:21:14 +0300

    Add files via upload

---
+++ Lab2.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    int x, y, a, b, length = 0;
+
+    printf("Fill blanks in _x + _ = _: ");
+    scanf_s("%d%d%d", &a, &b, &y);
+
+    x = (y - b) / a;
+    do
+    {
+        x /= 10;
+        length++;
+    } while (x >= 10);
+
+    printf("%d", (y-b)/a);
+    for (int i = 0; i < 15 - length; i++)
+        printf("*");
+
+    return 0;
+}
